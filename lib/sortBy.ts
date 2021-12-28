483f154d4f4c5f488aa5959282968a31a5f01bd9
Park Kiwook <fender.wook1002@gmail.com>
2021-12-28 15:58:53 +0900

    feat: implement sortBy

---
+++ lib/sortBy.ts
@@ -0,0 +1,31 @@
+type SortBy = <T extends object>(property: keyof T) => (a: T, b: T) => number;
+
+/**
+ * 
+ * @param property Object Property
+ * @returns -1 | 0 | 1
+ * 
+ * @example
+ * ```ts
+ * const people = [
+ *   { firstName: "aaFirstName", lastName: "ccLastName" },
+ *   { firstName: "bbFirstName", lastName: "bbLastName" },
+ *   { firstName: "ccFirstName", lastName: "aaLastName" },
+ * ]
+ *
+ * people.sort(sortBy("lastName"))
+ * ```
+ * 
+ * @result
+ * ```ts
+ * [
+ *   { firstName: 'ccFirstName', lastName: 'aaLastName' },
+ *   { firstName: 'bbFirstName', lastName: 'bbLastName' },
+ *   { firstName: 'aaFirstName', lastName: 'ccLastName' }
+ * ]
+ * ```
+ */
+const sortBy: SortBy = (property) => (a, b) =>
+  a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
+
+export default sortBy;
